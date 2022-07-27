f9bf4264b864a8f969ae4a7446ac835993425922
MaryGas <marygt182@gmail.com>
2022-07-27 12:17:32 +0100

    First version Added menu button

--- app/src/main/java/android/example/foods/MainActivity.kt
+++ app/src/main/java/android/example/foods/MainActivity.kt
@@ -3,6 +3,7 @@ package android.example.foods
 import android.example.foods.adapter.ItemAdapter
 import android.example.foods.data.Datasource
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,6 +19,14 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
 
         recyclerView.adapter = ItemAdapter(this, myDataset)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        return true
 
     }
 }
